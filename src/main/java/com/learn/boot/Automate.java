ca5471c73243c6f07f1a0fb9684932807735a639
memorytao <memorytao@gmail.com>
2023-03-26 00:30:11 +0700

    can not text into textarea fix later

--- src/main/java/com/learn/boot/Automate.java
+++ src/main/java/com/learn/boot/Automate.java
@@ -1,8 +1,16 @@
 package com.learn.boot;
 
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+
+import io.opentelemetry.api.internal.StringUtils;
 
 public class Automate {
 
@@ -13,13 +21,27 @@ public class Automate {
             System.setProperty("webdriver.edge.driver", "D:\\code\\edgedriver_win64\\msedgedriver.exe");
 
             WebDriver webDriver = new EdgeDriver();
-
             webDriver.get("https://www.google.com/");
-            webDriver.findElement(By.name("q")).sendKeys("python");
-            webDriver.findElement(By.name("btnK")).submit();
+
+            webDriver.findElement(By.name("q")).sendKeys("python" + Keys.ENTER);
             webDriver.findElement(By.partialLinkText("Python Tutorial - W3Schools")).click();
 
-            webDriver.findElement(By.partialLinkText("Try it Yourself »")).click();
+            // ========================================================================================
+            webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+            webDriver.findElement(By.xpath("//a[normalize-space()='Try it Yourself »']")).click();
+            webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+
+            // ========================================
+
+            List<String> tabs = new ArrayList<>(webDriver.getWindowHandles());
+            webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+            webDriver.switchTo().window(tabs.get(1));
+
+            webDriver.findElement(By.xpath("/html[1]/body[1]/div[4]/div[3]/div[1]/div[1]")).sendKeys(" fgfdsjghjsdkfghkj ");
+
+            if (StringUtils.isNullOrEmpty(new Scanner(System.in).nextLine())) {
+                webDriver.quit();
+            }
         } catch (Exception e) {
             // TODO: handle exception
             e.printStackTrace();
